cae64415fbc46c86a7775d6fa5704b44e114d041
Júlia Pôrto <juportob@gmail.com>
2021-03-15 18:49:44 -0300

    Requisito 3

---
+++ front-end/src/Pages/Products.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MenuTop from '../components/MenuTop';
+
+export default function Products() {
+  return (
+    <div>
+      <MenuTop />
+    </div>
+  );
+}
--- front-end/src/Pages/Rotas.js
+++ front-end/src/Pages/Rotas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
+import Products from './Products';
 
 export default function Rotas() {
   return (
@@ -10,6 +11,7 @@ export default function Rotas() {
         <Route exact path="/"><Redirect to="/login" /></Route>
         <Route path="/login" component={ Login } />
         <Route path="/register" component={ Register } />
+        <Route path="/products" component={ Products } />
       </Switch>
     </BrowserRouter>
   );
