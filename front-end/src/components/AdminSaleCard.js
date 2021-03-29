9de3d8c76aee2ccba37def24cbc5eaf83d894286
mottak <kissylamottapereira@gmail.com>
2021-03-29 18:23:58 -0300

    borda pag pedidos

--- front-end/src/components/AdminSaleCard.js
+++ front-end/src/components/AdminSaleCard.js
@@ -7,7 +7,7 @@ function AdminSaleCard({ indexId, saleId, street, number, totalValue, status }) 
   return (
     <div className="d-flex flex-sm-column justify-content-sm-between mx-5 orders-links ">
       <h4 data-testid={ `${indexId}-order-number` }>{`Pedido ${saleId}`}</h4>
-      <div className="border border-bottom rounded my-3 shadow-sm btn-group sale-card">
+      <div className="my-3 shadow-sm btn-group sale-card">
         <div
           className="d-flex flex-fill flex-sm-row
           justify-content-sm-between m-2 btn-group mr-2"
