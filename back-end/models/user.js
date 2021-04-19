d6824b354ebbf15e5df3af880fb89373960af697
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-04-19 16:07:08 -0300

    Feat: Implementaçao inicial sequelize incompleta

---
+++ back-end/models/user.js
@@ -0,0 +1,12 @@
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = createUser;
