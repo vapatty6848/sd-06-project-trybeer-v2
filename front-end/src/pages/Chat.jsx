cb2f144f3756e61d1cc547ce109aa9e97330b750
danilocouto1 <dansantosalfa@gmail.com>
2021-04-20 17:46:05 -0300

    Button request 5-7

---
+++ front-end/src/pages/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuTop from '../components/MenuTop';
+
+function Chat() {
+  return (
+    <div>
+      <MenuTop title="Trybeer" />
+    </div>
+  );
+}
+
+export default Chat;
