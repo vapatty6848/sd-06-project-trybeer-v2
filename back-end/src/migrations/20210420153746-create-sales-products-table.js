07ca66401a8790c6f11b8c5e00801905cb999c8b
rafMewtwo <ponciano.raf@gmail.com>
2021-04-22 15:21:55 -0300

    last updatex2

---
+++ back-end/src/migrations/20210420153746-create-sales-products-table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('SalesProducts', {
+      saleId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: { model: 'Sales', key: 'id'},
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: { model: 'Products', key: 'id'},
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+  
+  down: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.dropTable('SalesProducts');
+  }
+};
