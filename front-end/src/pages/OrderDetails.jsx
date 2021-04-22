eef117b10531fc20e888f365a7ca2628afa7d575
deko possas <andrepossaster@gmail.com>
2021-04-22 20:22:45 -0300

    order funcionando e admin ainda nao

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -5,12 +5,14 @@ import MenuTop from '../components/MenuTop';
 import '../styles/orderDetails.css';
 export default function OrderDetails() {
   const { id } = useParams();
+  const [products, setProducts] = useState([]);
   const [productsOfSale, setProductsOfSale] = useState([]);
   console.log(productsOfSale);
 
   const fetchApiProductOfSale = async (idFetch) => {
     const productDetails = await api.fetchSaleProduct(idFetch);
     setProductsOfSale(productDetails);
+    setProducts(productDetails.products);
   };
   useEffect(() => {
     fetchApiProductOfSale(id);
@@ -26,7 +28,7 @@ export default function OrderDetails() {
     const day = date.slice(eigth, fourteen);
     return `${day}/${month}`;
   };
-  const sumOfCart = (sum, product) => sum + product.quantity * product.price;
+  const sumOfCart = (sum, product) => sum + product.salesProducts.quantity * product.price;
   return (
     <div>
       <div>
@@ -35,17 +37,17 @@ export default function OrderDetails() {
       <div className="en-title">
         <h2 data-testid="order-number">{ `Pedido ${id}` }</h2>
         <h2 data-testid="order-date">
-          {productsOfSale.length !== 0 && formatDate(productsOfSale[0].saleDate)}
+          {productsOfSale.length !== 0 && formatDate(productsOfSale.saleDate)}
         </h2>
       </div>
       <div className="main-container">
-        {productsOfSale.map((produto, index) => (
+        {products.map((produto, index) => (
           <div className="cada-compra" key={ produto.id }>
             <div className="compra">
               <div data-testid={ `${index}-product-qtd` }>{produto.quantity}</div>
               <div data-testid={ `${index}-product-name` }>{produto.name}</div>
               <div data-testid={ `${index}-product-total-value` }>
-                {(produto.price * produto.quantity).toLocaleString('pt-BR', {
+                {(produto.price * produto.salesProducts.quantity).toLocaleString('pt-BR', {
                   style: 'currency',
                   currency: 'BRL',
                 })}
@@ -55,7 +57,7 @@ export default function OrderDetails() {
         ))}
       </div>
       <h2 data-testid="order-total-value" className="total-price">
-        {productsOfSale
+        {products
           .reduce(sumOfCart, 0)
           .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
       </h2>
