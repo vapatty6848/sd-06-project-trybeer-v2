0d3a46ad1976a80e4b36b1b027887af098f87f34
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-04-22 08:43:40 -0300

    sequelize migrations

---
+++ back-end/migrations/20210422104026-create-products-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      urlImage: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ back-end/migrations/20210422110813-create-sales-table.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      deliveryAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'users', key: 'id' },
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        type: Sequelize.DATE,
+        name: 'saleDate',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
