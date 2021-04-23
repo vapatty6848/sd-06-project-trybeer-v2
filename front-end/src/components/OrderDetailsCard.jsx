1387141eb1a9a1ff179998b6ae91ca2ab52ca5db
Alvaro Neto <alvaroshn@gmail.com>
2021-04-23 10:15:40 -0300

    eslint [1]

--- front-end/src/components/OrderDetailsCard.jsx
+++ front-end/src/components/OrderDetailsCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ProductCard from './ProductCard';
 import currencyFormat from '../utils/currencyFormat';
 import convertData from '../utils/convertData';
 
-function OrderDetailsCard({orderDetails}) {
+function OrderDetailsCard({ orderDetails }) {
   if (orderDetails.length > 0) {
     return (
       <div>
@@ -17,20 +17,20 @@ function OrderDetailsCard({orderDetails}) {
           data do pedido:
           {convertData(orderDetails[0].saleDate)}
         </p>
-        <ProductCard product={ orderDetails }  />
+        <ProductCard product={ orderDetails } />
         <p data-testid="order-total-value">
           total do pedido:
           {' '}
-          {currencyFormat(Number(orderDetails.reduce(((acc,cur) => acc + cur.totalPrice),0)))}
+          {currencyFormat(Number(orderDetails.reduce(((acc, cur) => acc + cur.totalPrice), 0)))}
           {' '}
         </p>
-        <p data-testid={ 'order-status' }>
+        <p data-testid="order-status">
           Status:
           {' '}
           {orderDetails[0].status}
         </p>
         <hr />
-        <Link to="/orders" >Voltar</Link>
+        <Link to="/orders">Voltar</Link>
       </div>);
   }
   return <p>...loading </p>;
