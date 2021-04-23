ea59aadca1288a109d15d8c25d83f13717d2c107
danilocouto1 <dansantosalfa@gmail.com>
2021-04-23 18:06:37 -0300

    LintFix

--- front-end/src/components/AdminListChats.jsx
+++ front-end/src/components/AdminListChats.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import admOrders from '../methods/admOrders';
 
+const dateFormat = require('dateformat');
+
 function AdminListChats() {
   const [orders, setOrders] = useState([]);
   const history = useHistory();
-  const dateFormat = require('dateformat');
-
   useEffect(() => {
     const fetchOrders = async () => {
       const user = JSON.parse(localStorage.getItem('user'));
@@ -20,24 +20,24 @@ function AdminListChats() {
 
   return (
     <div>
-    <div>
-      {orders.length === 0
-        ? <p data-testid="text-for-no-conversation">Nenhuma conversa por aqui!</p>
-        : orders.map(({email, date}, i) => (
-          <div
-            data-testid="containerChat"
-            key={ i }
-            className="paracss"
-          >
-            <p data-testid="profile-name">{email}</p>
-            <p data-testid="last-message">
-              Última mensagem às
-              {dateFormat(date, 'HH:MM')}
-            </p>
-          </div>
-        ))}
+      <div>
+        {orders.length === 0
+          ? <p data-testid="text-for-no-conversation">Nenhuma conversa por aqui!</p>
+          : orders.map(({ email, date }, i) => (
+            <div
+              data-testid="containerChat"
+              key={ i }
+              className="paracss"
+            >
+              <p data-testid="profile-name">{email}</p>
+              <p data-testid="last-message">
+                Última mensagem às
+                {dateFormat(date, 'HH:MM')}
+              </p>
+            </div>
+          ))}
+      </div>
     </div>
-  </div>
   );
 }
 
