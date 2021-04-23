c43de9698d6a74473ee5e6fc54d3366dbb4f3fc2
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-23 18:15:42 -0300

    req 7

--- front-end/src/components/SideBarAdmin/SideBarAdmin.js
+++ front-end/src/components/SideBarAdmin/SideBarAdmin.js
@@ -11,6 +11,11 @@ function SideBarAdmin() {
   return (
     <nav className="admin-side-bar-container">
       <h1>TryBeer</h1>
+      <Link to="/admin/chats">
+        <button type="button" data-testid="side-menu-item-chat" className="botoes">
+          Conversas
+        </button>
+      </Link>
       <Link to="/admin/orders">
         <button type="button" data-testid="side-menu-item-orders" className="botoes">
           Pedidos
