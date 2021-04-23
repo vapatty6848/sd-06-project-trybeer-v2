eb653b6c41c4f06bfb374ec50732c5f3535b12ee
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-04-23 17:55:40 -0300

    setup inicial

--- front-end/src/pages/UserChat.js
+++ front-end/src/pages/UserChat.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import TopBar from '../components/SideBarClient/TopBar';
+import FormMessage from '../components/Chat/FormMessage';
 
 function UserChat() {
   return (
-    <div>Chat do usuário</div>
+    <div>
+      <TopBar title="TryBeer" />
+      Chat do usuário
+      <FormMessage />
+    </div>
   );
 }
 
