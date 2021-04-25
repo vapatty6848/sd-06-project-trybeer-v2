482792a2cc92233d932f221f25c651bdd148a45c
rafMewtwo <ponciano.raf@gmail.com>
2021-04-25 19:52:21 -0300

    addx6

--- front-end/src/pages/ChatAdmin.js
+++ front-end/src/pages/ChatAdmin.js
@@ -19,7 +19,7 @@ function ChatAdmin() {
   return (
     <div>
       {chatList.map((element, index) => (
-        <Link to="/admin/chat" key={ `link-${index} `}>
+        <Link to="/admin/chat" key={ `link-${index} ` }>
           <div key={ index } data-testid="containerChat">
             <h2 data-testid="profile-name">{element.user}</h2>
             <h3 data-testid="last-message">
