e50a3f1604536e4e5613b9f5c4025137adac1467
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-26 15:27:29 -0300

    checking

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const moment = require('moment');
-const http = require('http');
-const socket = require('socket.io');
 const LoginController = require('./src/controller/LoginControler');
 const UsersController = require('./src/controller/UsersController');
 const ProductsController = require('./src/controller/ProductsController');
@@ -15,34 +13,34 @@ const ClientOrdersController = require('./src/controller/ClientOrdersController'
 const PORT = 3001;
 
 const app = express();
-const httpServer = http.createServer(app);
+const httpServer = require('http').createServer(app);
 
 app.use(express.json());
 app.use(cors());
 
-const io = socket(httpServer, {
+const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
+    methods: ['GET', 'POST']
+  }
 });
 
-io.on('connection', (socketIo) => {
+io.on('connection', (socket) => {
   console.log('Usuário conectado');
 
-  socketIo.on('connectRoom', (roomName) => {
-    socketIo.join(roomName);
+  socket.on('connectRoom', (roomName) => {
+    socket.join(roomName);
   });
 
-  socketIo.on('teste', (msg) => {
+  socket.on('teste', (msg) => {
     console.log(`Server recebe mensagem: ${msg}`);
   });
 
-  socketIo.on('chat.sendMessage', (data) => {
+  socket.on('chat.sendMessage', (data) => {
     const sentAt = moment().format('HH:mm');
-    const dataIo = { ...data, sentAt };
-    console.log(dataIo);
-    io.to(dataIo.from).emit('chat.receiveMessage', dataIo);
+    data = { ...data, sentAt };
+    console.log(data);
+    io.to(data.from).emit('chat.receiveMessage', data);
   });
 });
 
