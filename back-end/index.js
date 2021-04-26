f4ba13d224bc58fe4f98435eb3059060c9d89dfb
FlavioSugano <flavio.sugano@gmail.com>
2021-04-26 19:24:50 -0300

    feat(back-end): requirement 6

--- back-end/index.js
+++ back-end/index.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+
+const port = 3001;
+const socketPort = 3000;
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: { origin: `http://localhost:${socketPort}`, methods: ['GET', 'POST'] },
+  });
+
 const path = require('path');
 const { json } = require('body-parser');
 const { usersRouter } = require('./controllers/users');
 const { productsRouter } = require('./controllers/products');
 const ordersRouter = require('./controllers/orders');
 const adminRouter = require('./controllers/admin');
-const userChatRouter = require('./controllers/chatUser');
-
-const app = express();
-const port = 3001;
+const userChatController = require('./controllers/chatUser');
 
 app.use(json());
 app.use(cors());
@@ -21,8 +27,19 @@ app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
 app.use('/admin', adminRouter);
-app.use('/chat', userChatRouter);
+
+io.on('connection', (socket) => {
+  console.log('connection do back-end');
+
+  socket.on('getMessages', ({ email }) => userChatController.getAllMessages({ email, io }));
+
+  socket.on('message', ({ nickname, message, email }) => (
+    userChatController.saveMessage({ nickname, message, email, io })
+  ));
+
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
 
 app.get('/', (_req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+httpServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
