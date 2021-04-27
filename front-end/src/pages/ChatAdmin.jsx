3166b553104a895ba42552ae72779e147658e883
Micaeli Teixeira <luime6@gmail.com>
2021-04-27 15:34:47 -0300

    feat(front-end): req 8

--- front-end/src/pages/ChatAdmin.jsx
+++ front-end/src/pages/ChatAdmin.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { SideBarAdm } from '../components';
+import socket from '../utils/socketClient';
 
 function ChatAdmin() {
+  const [chatMessages, setChatMessages] = useState([]);
+  const { email, role } = JSON.parse(localStorage.getItem('user'));
+
+  useEffect(() => {
+    socket.emit('getMessages', { email });
+  }, []);
+
+  useEffect(() => {
+    socket.on('message', (data) => {
+      setChatMessages([...data]);
+      console.log('chat message admin', chatMessages);
+    });
+  }, [chatMessages]);
   return (
-    <div> Pag. ChatsAdmin  </div>
+    <div>
+      <SideBarAdm />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1> oi</h1>
+    </div>
   );
-}
+
+};
 
 export default ChatAdmin;
