b45dfc605dd1440af7ef9e402d3ae70ec7c2c3a2
ricardorosa-dev <ricardorosa@gmail.com>
2021-04-27 22:57:47 -0300

    Um margin-left na pagina do chat tava impedindo o teste de ver o botao

--- front-end/src/pages/Chats/ListaDeConversas.js
+++ front-end/src/pages/Chats/ListaDeConversas.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../../components/Header/Header';
+// import Header from '../../components/Header/Header';
 
 export default function ListaDeConversas() {
   const [users, setUsers] = useState([]);
@@ -36,7 +36,6 @@ export default function ListaDeConversas() {
             ))}
           </div>
         )}
-      <Header title="TryBeer" user="admin" />
     </div>
-  );
+  )
 }
