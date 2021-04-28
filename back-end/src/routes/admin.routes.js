ded5f8257b3b7348fcc3dd57af211abad84b4f17
Cyrano webdev <cyrano@cyranowebdev.com>
2021-04-28 15:37:10 -0300

    styles(backend): admin routes

--- back-end/src/routes/admin.routes.js
+++ back-end/src/routes/admin.routes.js
@@ -6,9 +6,7 @@ const middlewares = require('../middlewares');
 const admin = express.Router();
 
 admin.get('/sales/:id', middlewares.authToken, middlewares.authAdmin, controllers.details);
-
 admin.get('/sales', middlewares.authToken, middlewares.authAdmin, controllers.sales);
-
 admin.put('/sales/:id', middlewares.authToken, middlewares.authAdmin, controllers.editSale);
 
 module.exports = admin;
