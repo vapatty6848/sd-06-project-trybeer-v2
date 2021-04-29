07dd228792f4b87c0a105cbd441aa162940c2a58
vapatty6848 <vapatty@gmail.com>
2021-04-29 11:37:42 -0300

    adicionando menu hamburguer

--- front-end/src/components/TopMenuAdmin.jsx
+++ front-end/src/components/TopMenuAdmin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import './componentsCSS/TopMenu.css';
 import { useHistory } from 'react-router-dom';
@@ -6,16 +6,19 @@ import { useHistory } from 'react-router-dom';
 export default function TopMenuAdmin({ pageTitle }) {
   const history = useHistory();
 
+  const [asideClass, setasideClass] = useState('aside-menu-off');
+
+  const handleOnClickHamburguerButton = () => {
+    const asideOn = 'aside-menu-on';
+    const asideOff = 'aside-menu-off';
+    if (asideClass === asideOn) setasideClass(asideOff);
+    if (asideClass === asideOff) setasideClass(asideOn);
+  };
+
   const handleClick = (e) => {
-    if (e.target.value === 'Pedidos') {
-      history.push('/admin/orders');
-    }
-    if (e.target.value === 'Perfil') {
-      history.push('/admin/profile');
-    }
-    if (e.target.value === 'Conversas') {
-      history.push('/admin/chats');
-    }
+    if (e.target.value === 'Pedidos') { history.push('/admin/orders'); }
+    if (e.target.value === 'Perfil') { history.push('/admin/profile'); }
+    if (e.target.value === 'Conversas') { history.push('/admin/chats'); }
     if (e.target.value === 'Sair') {
       localStorage.setItem('token', '');
       history.push('/login');
@@ -23,43 +26,63 @@ export default function TopMenuAdmin({ pageTitle }) {
   };
 
   return (
-    <aside className="admin-side-bar-container">
-      <h1 data-testid="top-menu">{pageTitle}</h1>
+    <header className="nav-main">
+      <div className="title-burguer-div">
       <button
-        type="button"
-        data-testid="side-menu-item-orders"
-        value="Pedidos"
-        onClick={ handleClick }
-      >
-        Pedidos
-      </button>
-      <button
-        type="button"
-        data-testid="side-menu-item-profile"
-        value="Perfil"
-        onClick={ handleClick }
-      >
-        Perfil
-      </button>
-      <div className="side-menu-container">
-        <button
           type="button"
-          data-testid="side-menu-item-chat"
-          onClick={ handleClick }
-          value="Conversas"
+          data-testid="top-hamburguer"
+          className="btn-toggle-nav"
+          onClick={ handleOnClickHamburguerButton }
         >
-          Conversas
+          &nbsp;
         </button>
-      </div>
-      <button
-        type="button"
-        data-testid="side-menu-item-logout"
-        value="Sair"
-        onClick={ handleClick }
-      >
-        Sair
-      </button>
+        <h1 data-testid="top-menu" className="page-title">{pageTitle}</h1>
+        <aside id="aside" className={ asideClass }>
+          <div className="side-menu-container">
+            <button
+              type="button"
+              data-testid="side-menu-item-orders"
+              value="Pedidos"
+              onClick={ handleClick }
+            >
+              Pedidos
+            </button>
+          </div>
+          <div className="side-menu-container">
+             <button
+              type="button"
+              data-testid="side-menu-item-profile"
+              value="Perfil"
+              onClick={ handleClick }
+            >
+              Perfil
+            </button>
+          </div>
+      
+          <div className="side-menu-container">
+            <button
+              type="button"
+              data-testid="side-menu-item-chat"
+              onClick={ handleClick }
+              value="Conversas"
+            >
+              Conversas
+            </button>
+          </div>
+          <div className="side-menu-container">
+            <button
+              type="button"
+              data-testid="side-menu-item-logout"
+              value="Sair"
+              onClick={ handleClick }
+           >
+              Sair
+          </button>
+        </div> 
     </aside>
+      </div>
+    </header>
+    
   );
 }
 
--- front-end/src/pages/CostumerChat.jsx
+++ front-end/src/pages/CostumerChat.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChatMessage from '../components/ChatMessage';
 import MessageBox from '../components/MessageBox';
+import TopMenu from '../components/TopMenu';
 // import socket from '../utils/socketClient';
 
 export default function CostumerChat() {
   return (
     <div>
+      <TopMenu pageTitle="Chat" />
       <MessageBox />
       <ChatMessage />
     </div>
